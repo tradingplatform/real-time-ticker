afa13c1a6d1269fe323e439b7fe25489157fccd7
James Kim <code4life@gmail.com>
2015-09-17 13:47:13 -0400

    initial check-ins

---
+++ MarketDataTestBench/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infusion.Trading.MarketData.Client;
+
+namespace MarketDataTestBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || !args.Any())
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Usage: MarketDataTestBench [snapshot] symbol symbol ...");
+                return;
+            }
+
+            using (var client = new ZmqClient())
+            {
+                if (args[0] == "snapshot")
+                {
+                    var response = client.GetQuoteInfo(args.Skip(1).ToArray());
+                    Console.WriteLine(response);
+                    Console.WriteLine();
+                    return;
+                }
+
+                client.Start(args);
+            }
+        }
+    }
+}
